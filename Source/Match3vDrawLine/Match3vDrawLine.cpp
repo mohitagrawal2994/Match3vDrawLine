f6dc49d378bd4098b8ac868c75ab2e1917b995d0
Mohit Agrawal <mohit.agrawal2994@gmail.com>
2020-11-03 22:27:26 +0530

    Initial commit

---
+++ Source/Match3vDrawLine/Match3vDrawLine.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Match3vDrawLine.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Match3vDrawLine, "Match3vDrawLine" );
---
+++ Source/Match3vDrawLine/Match3vDrawLineGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Match3vDrawLineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATCH3VDRAWLINE_API AMatch3vDrawLineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
